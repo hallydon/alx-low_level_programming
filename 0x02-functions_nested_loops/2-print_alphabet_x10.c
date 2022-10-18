d8c67823c10e733c93b439c7d1f4bfd46a3b117a
Halidu Ibrahim <hallydon006@gmail.com>
2022-10-18 16:57:07 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 time in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int x;
+
+	char c;
+
+	for (x = 0; x <= 10; x++)
+	{
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+		_putchar('\n');
+	}
+
+}
+
+
