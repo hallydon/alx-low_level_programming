294dfd0c0db647f021ec18b3c943a4742e97d8fb
Halidu Ibrahim <hallydon006@gmail.com>
2022-10-28 01:25:14 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,7 @@
 
 /**
  * cap_string - capitalizes words
+ OB
  * @str: string
  * Return: pointer to string
  */
@@ -11,9 +12,9 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		    while (!(str[i] >= 'a' && str[i] <= 'z'))
-			    i++;
-		    if (str[i - 1] == ' ' ||
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
 			str[i - 1] == ',' ||
@@ -27,10 +28,8 @@ char *cap_string(char *str)
 			str[i - 1] == '{' ||
 			str[i - 1] == '}' ||
 			i == 0)
-			   str[i] -= 32;
-
-		    i++;
+			str[i] -= 32;
+			i++;
 	}
-
 	return (str);
 }
