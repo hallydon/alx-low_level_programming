b155c144847008de3160cfaa8357241c37ebdd6a
Halidu Ibrahim <hallydon006@gmail.com>
2022-11-09 06:39:21 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry prints name of progarm
+ * @argc: counts number of argument
+ * @argv: points to the argument passed
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("name of the program is  %s\n")
+
+		return (0);
+}
+
