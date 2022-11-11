bca1697cd5c3f4c75dd5a8b4692f0afb74a115b1
Halidu Ibrahim <hallydon006@gmail.com>
2022-11-11 03:08:54 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: counts number of argument
+ * @argv: an array of command list
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("argv index %d is = %s\n", count, argv[count]);
+	return (0);
+}
