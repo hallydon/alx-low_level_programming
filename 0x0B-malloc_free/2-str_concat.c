db247fd5ce74726750b4d4bff7f745d29c1c704b
Halidu Ibrahim <hallydon006@gmail.com>
2022-12-04 01:04:27 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	total = (_strlen(s1) + _strlen(s2) + 1);
 	new = (char *) malloc((total * sizeof(char)));
 
@@ -47,7 +47,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(new + i) = *(s1 + i);
-	
+
 	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
 		*(new + i) = *(s2 + j);
