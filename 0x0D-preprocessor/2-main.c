8b123692a313be881a7a50b7dc4708811c15f7c8
Halidu Ibrahim <hallydon006@gmail.com>
2022-12-30 02:04:09 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -prints the name of the file if the program
+ * was compiled from , followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
